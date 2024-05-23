8ff44469013ca1ed7ffe5fe94cd6443ee17af833
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-23 17:32:17 -0300

    Adicionado ModeloDeResposta para gerennciar mensagens de resposta dentro do sistema

---
+++ src/main/java/com/higor/sistema_de_busca_de_carros/Entities/ModeloDeResposta.java
@@ -0,0 +1,14 @@
+package com.higor.sistema_de_busca_de_carros.Entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class ModeloDeResposta {
+
+    private String mensagem;
+
+}
