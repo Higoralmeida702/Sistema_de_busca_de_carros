d9009de0b7672ca19a5ff8d72a380a2ce7fb32a5
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-23 14:56:15 -0300

    Inicializção de projeto CRUD

---
+++ src/main/java/com/higor/sistema_de_busca_de_carros/SistemaDeBuscaDeCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.higor.sistema_de_busca_de_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBuscaDeCarrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeBuscaDeCarrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/higor/sistema_de_busca_de_carros/SistemaDeBuscaDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higor.sistema_de_busca_de_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBuscaDeCarrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
