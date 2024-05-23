7544b2f224eceb09746f9e1cfec33f74d607eb86
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-23 17:28:27 -0300

    Adicionado sistema de cadastramento
    -Adicionado linha de codigo base para a busca de veiculos para melhor tramento mais tarde.

---
+++ src/main/java/com/higor/sistema_de_busca_de_carros/Controller/InfoCarroController.java
@@ -0,0 +1,36 @@
+package com.higor.sistema_de_busca_de_carros.Controller;
+
+import com.higor.sistema_de_busca_de_carros.Entities.InfoCarro;
+import com.higor.sistema_de_busca_de_carros.Service.InfoCarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin (origins = "*")
+public class InfoCarroController {
+
+    @Autowired
+    private InfoCarroService infoCarroService;
+
+    @GetMapping("/buscarPorCodigoVeiculo")
+        public ResponseEntity<?> buscarPorCodigoVeiculo (@PathVariable String codigoDoVeiculo){
+            try {
+                Optional<InfoCarro> codigoVeiculo = infoCarroService.buscarPorCodigoVeiculo(codigoDoVeiculo);
+                if (codigoVeiculo.isPresent()) {
+                    return ResponseEntity.ok(codigoVeiculo.get());
+                }else {
+                    return ResponseEntity.notFound().build();
+                }
+            }catch (Exception e) {
+                return ResponseEntity.badRequest().body("O codigo do veiculo não foi encontrado" + e.getMessage());
+            }
+        }
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody InfoCarro infoCarro){
+        return infoCarroService.cadastrar(infoCarro, "cadastrar");
+    }
+
+}
---
+++ src/main/java/com/higor/sistema_de_busca_de_carros/Service/InfoCarroService.java
@@ -0,0 +1,37 @@
+package com.higor.sistema_de_busca_de_carros.Service;
+
+import com.higor.sistema_de_busca_de_carros.Entities.InfoCarro;
+import com.higor.sistema_de_busca_de_carros.Entities.ModeloDeResposta;
+import com.higor.sistema_de_busca_de_carros.Repository.InfoCarroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class InfoCarroService {
+
+    @Autowired
+    private InfoCarroRepository infoCarroRepository;
+
+    @Autowired
+    private ModeloDeResposta modeloResposta;
+
+    public Optional<InfoCarro> buscarPorCodigoVeiculo (String codigoDoVeiculo){
+        return infoCarroRepository.findByCodigoDoVeiculo (codigoDoVeiculo);
+    }
+
+    public ResponseEntity<?> cadastrar(InfoCarro infoCarro, String acao) {
+        if (infoCarro.getProprietario().isEmpty()) {
+            return new ResponseEntity<ModeloDeResposta>(modeloResposta, HttpStatus.BAD_REQUEST);
+        }else {
+            if (acao.equals("cadastrar")) {
+                return new ResponseEntity<InfoCarro>(infoCarroRepository.save(infoCarro), HttpStatus.CREATED);
+            }else {
+                return new ResponseEntity<InfoCarro>(infoCarroRepository.save(infoCarro), HttpStatus.OK);
+            }
+        }
+    }
+}
