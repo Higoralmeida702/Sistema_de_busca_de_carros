9738e172af27a00e2420fd1fbbd13353e2ebef15
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-05-26 13:54:36 -0300

    Adicionar funcionalidade de consulta de veículos;
    - Adiciona a função getPosts para fazer uma chamada assíncrona à API e obter os dados dos posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <></>;
+   const [posts, setPosts] = useState([]);
+
+   const getPosts = async () => {
+      try {
+         const response = await axios.get(
+            'http://localhost:8080/listarInformacoes'
+         );
+         const data = response.data;
+         setPosts(data);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   useEffect(() => {
+      getPosts();
+   }, []);
+   return (
+      <div>
+         <h1>Consultar veiculo</h1>
+      </div>
+   );
 }
 
 export default App;
