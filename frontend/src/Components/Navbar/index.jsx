04262e7e21da0accd0e14e0fee25f21653799853
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-06-13 21:25:48 -0300

    Adicionado estilo css da barra de navegação

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -1,14 +1,15 @@
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
    return (
-      <nav>
+      <nav className="navbar">
          <ul>
             <li>
-               <Link to={'/'}>Consultar veiculo</Link>
+               <Link to={'/'}>Consultar</Link>
             </li>
             <li>
-               <Link to={'/cadastrar'}>Cadastrar</Link>
+               <Link to={'/cadastrar'}>Cadastrar Aluno</Link>
             </li>
          </ul>
       </nav>
